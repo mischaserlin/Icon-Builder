0b448b8e8a13beafabdbeca2a6af640c5692bdcf
Mischa Serlin <github@mischaserlin.com>
2012-08-25 16:13:23 -0700

    reposition images using top and left

--- icon_builder.js
+++ icon_builder.js
@@ -3,9 +3,9 @@ $(function(){
 		, selectedCategory;
 
 	var imageCategories = {
-		backgrounds: [{id: 0, name: "Red", opacity: 1, url: "images/backgrounds/red.png"}, {id: 1, name: "Orange", opacity: 1, url: "images/backgrounds/orange.png"}, {id: 2, name: "Yellow", opacity: 1, opacity: 1, url: "images/backgrounds/yellow.png"}, {id:3, name:"Green", opacity: 1, opacity: 1, url: "images/backgrounds/green.png"}, {id: 4, name: "Blue", opacity: 1, url: "images/backgrounds/blue.png"}, {id: 5, name: "Magenta", opacity: 1, url: "images/backgrounds/magenta.png"}, {id: 6, name: "Violet", opacity: 1, url: "images/backgrounds/violet.png"}, {id: 7, name: "White", opacity: 1, url: "images/backgrounds/white.png"}, {id: 8, name: "Grey", opacity: 1, url: "images/backgrounds/grey.png"}, {id: 9, name: "Black", opacity: 1, url: "images/backgrounds/black.png"}]
-		, icons: [{id: 10, name: "Chart", opacity: 1, url: "images/icons/chart.png"}, {id: 11, name: "Chatbox", opacity: 1, url: "images/icons/chatbox.png"}, {id: 12, name: "Check", opacity: 1, url: "images/icons/check.png"}, {id: 13, name: "Cloud", opacity: 1, url: "images/icons/cloud.png"}, {id: 14, name: "Eye", opacity: 1, url: "images/icons/eye.png"}, {id: 15, name: "Music", opacity: 1, url: "images/icons/music.png"}, {id: 16, name: "Plane", opacity: 1, url: "images/icons/plane.png"}, {id: 17, name: "Play", opacity: 1, url: "images/icons/play.png"}, {id: 18, name: "Scissors", opacity: 1, url: "images/icons/scissors.png"}]
-		, effects: [{id: 20, name: "Highlight", opacity: 1, url: "images/effects/lighting.png"}, {id: 21, name: "Round Edges", opacity: 1, url: "images/effects/rounded_edges.png"}]
+		backgrounds: [{id: 0, name: "Red", opacity: 1, size: 144, top: 0, left: 0, url: "images/backgrounds/red.png"}, {id: 1, name: "Orange", opacity: 1, size: 144, top: 0, left: 0, url: "images/backgrounds/orange.png"}, {id: 2, name: "Yellow", opacity: 1, opacity: 1, size: 144, top: 0, left: 0, url: "images/backgrounds/yellow.png"}, {id:3, name:"Green", opacity: 1, opacity: 1, size: 144, top: 0, left: 0, url: "images/backgrounds/green.png"}, {id: 4, name: "Blue", opacity: 1, size: 144, top: 0, left: 0, url: "images/backgrounds/blue.png"}, {id: 5, name: "Magenta", opacity: 1, size: 144, top: 0, left: 0, url: "images/backgrounds/magenta.png"}, {id: 6, name: "Violet", opacity: 1, size: 144, top: 0, left: 0, url: "images/backgrounds/violet.png"}, {id: 7, name: "White", opacity: 1, size: 144, top: 0, left: 0, url: "images/backgrounds/white.png"}, {id: 8, name: "Grey", opacity: 1, size: 144, top: 0, left: 0, url: "images/backgrounds/grey.png"}, {id: 9, name: "Black", opacity: 1, size: 144, top: 0, left: 0, url: "images/backgrounds/black.png"}]
+		, icons: [{id: 10, name: "Chart", opacity: 1, size: 144, top: 0, left: 0, url: "images/icons/chart.png"}, {id: 11, name: "Chatbox", opacity: 1, size: 144, top: 0, left: 0, url: "images/icons/chatbox.png"}, {id: 12, name: "Check", opacity: 1, size: 144, top: 0, left: 0, url: "images/icons/check.png"}, {id: 13, name: "Cloud", opacity: 1, size: 144, top: 0, left: 0, url: "images/icons/cloud.png"}, {id: 14, name: "Eye", opacity: 1, size: 144, top: 0, left: 0, url: "images/icons/eye.png"}, {id: 15, name: "Music", opacity: 1, size: 144, top: 0, left: 0, url: "images/icons/music.png"}, {id: 16, name: "Plane", opacity: 1, size: 144, top: 0, left: 0, url: "images/icons/plane.png"}, {id: 17, name: "Play", opacity: 1, size: 144, top: 0, left: 0, url: "images/icons/play.png"}, {id: 18, name: "Scissors", opacity: 1, size: 144, top: 0, left: 0, url: "images/icons/scissors.png"}]
+		, effects: [{id: 20, name: "Highlight", opacity: 1, size: 144, top: 0, left: 0, url: "images/effects/lighting.png"}, {id: 21, name: "Round Edges", opacity: 1, size: 144, top: 0, left: 0, url: "images/effects/rounded_edges.png"}]
 	};
 
 	var sourceLayers = $("#imageLayerTemplate").html();
@@ -88,7 +88,7 @@ $(function(){
 				break;
 			}
 		}
-		// imageSelected = {id: imageSelected_id, name: imageSelected_name, opacity: 1, url: imageSelected_url}
+		// imageSelected = {id: imageSelected_id, name: imageSelected_name, opacity: 1, size: 144, top: 0, left: 0, url: imageSelected_url}
 
 		$('#imageCategory li').removeClass('highlight');
 		$(this).addClass("highlight");
@@ -97,7 +97,7 @@ $(function(){
 			id: imageSelected.id
 			, opacity: imageSelected.opacity
 			, name: imageSelected.name
-			, url: imageSelected.url
+			, size: 144, top: 0, left: 0, url: imageSelected.url
 		});
 		console.log(imageLayers);
 
@@ -143,7 +143,76 @@ $(function(){
 				break;
 			}
 		}
-		// $('.imageopacity-field').val(setOpacity);
+		// $('.imageOpacityField').val(setOpacity);
+
+		showPreview();
+	};
+
+	var changeSize = function(e){
+		var setSize;
+		
+		if(Number($(this).val()) == NaN){
+			alert("Please enter a valid number.")
+			return;
+		}
+			
+		setSize = $(this).val();
+
+		console.log(setSize);
+
+		var selectedImageLayerId = $(this).attr("data-id");
+		for(var i = 0; i < imageLayers.length; i++){
+			if( selectedImageLayerId == imageLayers[i].id ){
+				imageLayers[i].size = setSize;
+				break;
+			}
+		}
+
+		showPreview();
+	};
+
+	var changeTopPosition = function(e){
+		var setTopPosition;
+		
+		if(Number($(this).val()) == NaN){
+			alert("Please enter a valid number.")
+			return;
+		}
+			
+		setTopPosition = $(this).val();
+
+		console.log(setTopPosition);
+
+		var selectedImageLayerId = $(this).attr("data-id");
+		for(var i = 0; i < imageLayers.length; i++){
+			if( selectedImageLayerId == imageLayers[i].id ){
+				imageLayers[i].top = setTopPosition;
+				break;
+			}
+		}
+
+		showPreview();
+	};
+
+	var changeLeftPostion = function(e){
+		var setLeftPosition;
+		
+		if(Number($(this).val()) == NaN){
+			alert("Please enter a valid number.")
+			return;
+		}
+			
+		setLeftPosition = $(this).val();
+
+		console.log(setLeftPosition);
+
+		var selectedImageLayerId = $(this).attr("data-id");
+		for(var i = 0; i < imageLayers.length; i++){
+			if( selectedImageLayerId == imageLayers[i].id ){
+				imageLayers[i].left = setLeftPosition;
+				break;
+			}
+		}
 
 		showPreview();
 	};
@@ -152,7 +221,15 @@ $(function(){
 
 	$('#imageLayersList').on('click', '.deletelayer', removeLayer);
 
-	$('#imageLayersList').on('keydown', '.imageopacity-field', changeOpacity);
+	$('#imageLayersList').on('keydown', '.imageOpacityField', changeOpacity);
+
+	$('#imageLayersList').on('keyup', '.imageSizeField', changeSize);
+	//later add ability to change width and height seperately with an option to link 
+	//them to continue to change them proportionately.
+
+	$('#imageLayersList').on('keyup', '.imageTopPositionField', changeTopPosition);
+
+	$('#imageLayersList').on('keyup', '.imageLeftPositionField', changeLeftPostion);
 
 	$('#imageLayersList').dragsort({ 
 		dragSelector: ".movelayer"
